979638a72976807d193bf077538ab482a31d860a
abhiraj-chatterjee <abhirajchatterjee@gmail.com>
2021-07-15 03:56:37 +0530

    subtab backend

--- models/subtab.js
+++ models/subtab.js
@@ -3,6 +3,14 @@ const { BadRequestError } = require("../utils/errors");
 
 class Subtab {
 
+    /* Fetch all subtabs */
+    static async listSubtabsForParent(id) {
+
+        const query =  `
+            SELECT * from subtabs
+            
+        `
+    }  
 }
 
 module.exports = Subtab;
