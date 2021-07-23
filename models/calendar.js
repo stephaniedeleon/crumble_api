cb328426efed92fcf4a8ac25e1490a2c35b7d191
stephaniedeleon <stephanie.deleon516@gmail.com>
2021-07-22 17:47:13 -0700

    added calendar backend

--- models/calendar.js
+++ models/calendar.js
@@ -3,6 +3,101 @@ const { BadRequestError } = require("../utils/errors");
 
 class Calendar {
 
+    /** Fetch all events for maintab */
+    static async listEventsForMaintab(main_id) {
+
+        const query = `
+            SELECT * FROM calendar
+            WHERE calendar.main_id = $1
+            ORDER BY created_at DESC;
+        `
+        const result = await db.query(query, [main_id]);
+
+        //return events
+        return result.rows;
+    }
+
+    /** Fetch all events for subtab */
+    static async listEventsForSubtab(sub_id) {
+
+        const query = `
+        SELECT * FROM calendar
+        WHERE calendar.sub_id = $1
+        ORDER BY created_at DESC;
+        `
+        const result = await db.query(query, [sub_id]);
+
+        //return events
+        return result.rows;
+    }
+
+    /** Creating a new event for maintab */
+    static async createEventForMaintab({ main_id, event }) {
+
+        const requiredFields = ["event_name", "date"];
+        requiredFields.forEach((field) => {
+            if (!event.hasOwnProperty(field) || !event[field]) {
+                throw new BadRequestError(`Required field - ${field} - missing from request body.`)
+            }
+        });
+
+        //create a new maintab - store in database
+        const query = `
+            INSERT INTO calendar (main_id, event_name, date)
+            VALUES ($1, $2, $3) 
+            RETURNING id, main_id, sub_id, event_name, date, created_at;            
+        `
+        const result = await db.query(query, [main_id, event.event_name, event.date]);
+
+        //return new event
+        return result.rows[0];
+    }
+
+    /** Creating a new event for subtab */
+    static async createEventForSubtab({ sub_id, event }) {
+
+        const requiredFields = ["event_name", "date"];
+        requiredFields.forEach((field) => {
+            if (!event.hasOwnProperty(field) || !event[field]) {
+                throw new BadRequestError(`Required field - ${field} - missing from request body.`)
+            }
+        });
+
+        //create a new maintab - store in database
+        const query = `
+            INSERT INTO calendar (sub_id, event_name, date)
+            VALUES ($1, $2, $3) 
+            RETURNING id, main_id, sub_id, event_name, date, created_at;            
+        `
+        const result = await db.query(query, [sub_id, event.event_name, event.date]);
+
+        //return new event
+        return result.rows[0];
+    }
+
+    /** Deleting an event */
+    static async deleteEvent(event_id) {
+        const query = `
+                DELETE FROM calendar
+                WHERE calendar.id = $1;
+            `;
+
+        const result = await db.query(query, [event_id]);
+
+        return result.rows;
+    }
+
 }
 
 module.exports = Calendar;
+
+// CREATE TABLE calendar (
+//     id              SERIAL PRIMARY KEY,
+//     main_id         INTEGER DEFAULT NULL,
+//     sub_id          INTEGER DEFAULT NULL,
+//     event_name      VARCHAR(20) NOT NULL,
+//     date            TIMESTAMP NOT NULL,
+//     created_at      TIMESTAMP NOT NULL DEFAULT NOW(),
+//     FOREIGN KEY     (main_id) REFERENCES main_tabs(id) ON DELETE CASCADE,
+//     FOREIGN KEY     (sub_id) REFERENCES subtabs(id) ON DELETE CASCADE
+// );
--- routes/calendars.js
+++ routes/calendars.js
@@ -1,6 +1,83 @@
 const express = require("express");
+const { requireAuthenticatedUser } = require("../middleware/security");
 const router = express.Router();
-const Calendar = require("../models/calendars");
+const Calendar = require("../models/calendar");
+
+/** Listing events for calendar for maintab */
+router.get("/main/:maintabId", requireAuthenticatedUser, async (req, res, next) => {
+
+    try {
+        const main_id = req.params.maintabId; 
+        const events = await Calendar.listEventsForMaintab(main_id);
+        res.status(200).json({ events });
+
+    } catch (error) {
+        next(error);
+    }
+});
+
+/** Listing events for calendar for subtab */
+router.get("/sub/:subtabId", requireAuthenticatedUser, async (req, res, next) => {
+
+    try {
+        const sub_id = req.params.subtabId;
+        const events = await Calendar.listEventsForSubtab(sub_id);
+        res.status(200).json({ events });
+
+    } catch (error) {
+        next(error);
+    }
+});
+
+/** Creating a new event in maintab */
+router.post("/main/create", requireAuthenticatedUser, async (req, res, next) => {
+
+    try {
+        const event = await Calendar.createEventForMaintab({ main_id: req.body.main_id, event: req.body.event });
+        res.status(201).json({ event });
+
+    } catch(err) {
+        next(err);
+    }
+});
+
+/** Creating a new event in subtab */
+router.post("/sub/create", requireAuthenticatedUser, async (req, res, next) => {
+
+    try {
+        const event = await Calendar.createEventForSubtab({ sub_id: req.body.sub_id, event: req.body.event });
+        res.status(201).json({ event });
+
+    } catch(err) {
+        next(err);
+    }
+});
+
+
+/** Deleting an event */
+router.delete("/:eventId", requireAuthenticatedUser, async (req, res, next) => {
+
+    try {
+        const event_id = req.params.eventId;
+        const event = await Calendar.deleteEvent(event_id);
+        res.status(201).json({ event });
+
+    } catch(err) {
+        next(err);
+    }
+});
 
 
 module.exports = router;
+
+
+// CREATE TABLE calendar (
+//     id          SERIAL PRIMARY KEY,
+//     main_id     INTEGER DEFAULT NULL,
+//     sub_id      INTEGER NOT NULL,
+//     event       VARCHAR(20) NOT NULL,
+//     date        TIMESTAMP NOT NULL,
+//     created_at  TIMESTAMP NOT NULL DEFAULT NOW(),
+//     FOREIGN KEY     (main_id) REFERENCES main_tabs(id) ON DELETE CASCADE,
+//     FOREIGN KEY (sub_id) REFERENCES subtabs(id) ON DELETE CASCADE
+// );
